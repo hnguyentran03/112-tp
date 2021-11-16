253b624bf1a78b64a2dce2d985462f87635cf803
Ben Nguyen <hnguyent@andrew.cmu.edu>
2021-11-16 16:47:56 -0500

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -8,22 +8,22 @@ def appStarted(app):
     pass
 
 def generateMaze(app):
-    rows = cols = 10
+    rows = cols = 20
     app.margin = 10
     app.cellMargin = 5
-    app.maze = Maze()
+    app.maze = Maze(app)
     app.maze.dfsMaze(rows, cols)
 
 
 def drawPath(app, canvas):
-    path = app.maze.getPath(app.player.checkLocation(app), (app.maze.rows-1, app.maze.cols-1))
+    path = app.maze.getPath(app.player.checkLocation(), (app.maze.rows-1, app.maze.cols-1))
     for row, col in path:
         
-        x0, x1, y0, y1 = app.maze.getCellBounds(app, row, col)
+        x0, x1, y0, y1 = app.maze.getCellBounds(row, col)
         cx, cy = (x1 + x0)/2, (y1 + y0)/2
 
         nrow, ncol = path[(row, col)]
-        nx0, nx1, ny0, ny1 = app.maze.getCellBounds(app, nrow, ncol)
+        nx0, nx1, ny0, ny1 = app.maze.getCellBounds(nrow, ncol)
         ncx, ncy = (nx1 + nx0)/2, (ny1 + ny0)/2
         canvas.create_line(cx, cy, ncx, ncy, fill = 'blue', width = 5)
 
@@ -37,7 +37,7 @@ def keyPressed(app, event):
         generateMaze(app)
 
     #Moving
-    app.player.moveWithKeys(app, event)
+    app.player.moveWithKeys(event)
 
 '''
 Timer
@@ -47,10 +47,10 @@ def timerFired(app):
 
 def redrawAll(app, canvas):
     canvas.create_rectangle(0, 0, app.width, app.height, fill = 'black')
-    app.maze.render(app, canvas)
+    app.maze.render(canvas)
     if app.path:
         drawPath(app, canvas)
-    app.player.render(app, canvas)
+    app.player.render(canvas)
 
 
-runApp(width=400,height=400)
+runApp(width=500,height=500)
