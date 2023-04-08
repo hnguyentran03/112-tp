e0ee07ffa87627488baf00cd16618c229e1789fd
Ben Nguyen Tran <hnguyentran03@gmail.com>
2023-04-07 21:06:44 -0400

    implemented prims and dfs maze gen

--- graph.py
+++ graph.py
@@ -1,8 +1,8 @@
 import random
 # from cmu_112_graphics import *
 
-class Node:
-    nodeTypes = ['wall', 'cell', 'key', 'goal']
+class Vertex:
+    vertexTypes = ['wall', 'cell', 'key', 'goal']
     def __init__(self, row, col, t, color):
         self.row = row
         self.col = col
@@ -10,7 +10,7 @@ class Node:
         self.color = color
         
     def __repr__(self):
-        return f"Node({self.row}, {self.col}, {self.t}, {self.color})"
+        return f"Vertex({self.row}, {self.col}, {self.t}, {self.color})"
     
     def getPos(self):
         return (self.row, self.col)
@@ -21,14 +21,14 @@ class Node:
     def getColor(self):
         return self.color
 
-class Wall(Node):
+class Wall(Vertex):
     def __init__(self, row, col, color):
         super().__init__(row, col, 'wall', color)
     
     def __repr__(self):
         return f"Wall({self.row}, {self.col}, {self.color})"
 
-class Cell(Node):
+class Cell(Vertex):
     def __init__(self, row, col, color):
         super().__init__(row, col, 'cell', color)
     
@@ -38,7 +38,7 @@ class Cell(Node):
 #Idea for graph class (and some funcitons) from Graph Algorithm Mini-Lecture
 class Graph:
     def __init__(self, wallColor='white', cellColor='white'):
-        self.table = {}
+        self.table = {} #Adjacency table
         self.cellColor = cellColor
         self.wallColor = wallColor
 
@@ -46,27 +46,28 @@ class Graph:
     def __repr__(self):
         return f'{self.table}'
     
-    def addEdge(self, nodeA, nodeB, weight=1):
-        if nodeA not in self.table:
-            self.table[nodeA] = {}
-        if nodeB not in self.table:
-            self.table[nodeB] = {}
-        self.table[nodeA][nodeB] = weight
-        self.table[nodeB][nodeA] = weight
+    #Adds a directed edge between vertexA and vertexB
+    def addEdge(self, vertexA, vertexB, weight=1):
+        if vertexA not in self.table:
+            self.table[vertexA] = {}
+        if vertexB not in self.table:
+            self.table[vertexB] = {}
+        self.table[vertexA][vertexB] = weight
+        # self.table[vertexB][vertexA] = weight
     
-    def getNodes(self):
+    def getVertexs(self):
         return list(self.table)
     
-    def getNeighbors(self, node):
-        return set(self.table.get(node, {}))
+    def getNeighbors(self, vertex):
+        return set(self.table.get(vertex, {}))
     
     def convertTo2DList(self):
         #All walls
         maze = [[Wall(row, col, self.wallColor) for col in range(self.cols*2-1)] for row in range(self.rows*2-1)]
         
         #Puts all cells into the maze
-        for node in self.table:
-            row, col = node.getPos()
+        for vertex in self.table:
+            row, col = vertex.getPos()
             maze[row*2][col*2] = Cell(row, col, self.cellColor)
 
         #Makes paths between cells
@@ -81,10 +82,10 @@ class Graph:
     def render(self, canvas):
         for row in range(len(self.L)):
             for col in range(len(self.L[0])):
-                node = self.L[row][col]
+                vertex = self.L[row][col]
                 canvas.create_rectangle(col*self.cellSize, row*self.cellSize, 
                                             (col+1)*self.cellSize, (row+1)*self.cellSize, 
-                                            fill=node.getColor())
+                                            fill=vertex.getColor())
 
 
 class Maze(Graph):
@@ -109,17 +110,46 @@ class Maze(Graph):
             self.generateIterDFS()
         elif self.mazeType == 'prim':
             self.generatePrim()
-        elif self.mazeType == 'kruskal':
-            self.generateKruskal()
+        # elif self.mazeType == 'kruskal':
+        #     self.generateKruskal()
         self.convertTo2DList()
         self.addGoals()
         self.addKey()
     
+    # Taken from https://en.wikipedia.org/wiki/Maze_generation_algorithm
     def generateIterDFS(self):
-        pass
+        visited = set()
+        frontier = [] #Stack
+        start = (random.randrange(0, self.rows), random.randrange(0, self.cols))
 
+        frontier.append(start)
+        while frontier:
+            vertex = row, col = frontier.pop()
+            visited.add(vertex)
+            for drow, dcol in [(0, 1), (0, -1), (1, 0), (-1, 0)]: #Right, Left, Down, Up
+                neighbor = nrow, ncol = (row + drow, col + dcol)
+                if 0 <= nrow < self.rows and 0 <= ncol < self.cols and  neighbor not in visited:
+                    self.addEdge(vertex, neighbor)
+                    self.addEdge(neighbor, vertex)
+                    frontier.append(neighbor)
+    
+    # Taken from https://en.wikipedia.org/wiki/Prim%27s_algorithm
     def generatePrim(self):
-        pass
+        visited = set()
+        frontier = set()
+        start = (random.randrange(0, self.rows), random.randrange(0, self.cols))
+
+        frontier.add(start)
+        while frontier:
+            vertex = row, col = random.choice(frontier)
+            frontier.remove(vertex)
+            visited.add(vertex)
+            for drow, dcol in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                neighbor = nrow, ncol = (row + drow, col + dcol)
+                if 0 <= nrow < self.rows and 0 <= ncol < self.cols and neighbor not in visited:
+                    frontier.add(neighbor)
+                    self.addEdge(vertex, neighbor)
+                    self.addEdge(neighbor, vertex)
 
     def generateKruskal(self):
         pass
@@ -130,12 +160,12 @@ class Maze(Graph):
     def render3D(self, canvas):
         pass
     
-    #TODO make gotKey
-    def renderMinimap(self, canvas, gotKey=False):
+    #TODO make getKey to modify maze
+    def renderMinimap(self, canvas):
         #Draws the minimap
         for row in range(len(self.L)):
             for col in range(len(self.L[0])):
-                node = self.L[row][col]
+                vertex = self.L[row][col]
                 canvas.create_rectangle(col*self.cellSize, row*self.cellSize, 
                                             (col+1)*self.cellSize, (row+1)*self.cellSize, 
-                                            fill=node.getColor())
+                                            fill=vertex.getColor())
